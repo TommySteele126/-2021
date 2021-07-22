c170177a1f93132c9908414046680473039946b3
NextGen45 <tommy48steele@gmail.com>
2021-07-22 13:42:40 -0700

    update

---
+++ project/hello.js
@@ -0,0 +1,10 @@
+
+
+//const name  ='Tommy';
+//const peson = 'Hey ${person}';
+//console.log(person);
+////const name = 'tommy'
+//const backticks = 'hello $(name) welcome';
+//console.log(backticks);
+
+
